4ad7352bc46f5b09ea562f540106d184fea3bc3d
Khải Nguyễn <khainguyentrantrung@gmail.com>
2024-07-21 15:28:30 +0700

    fix: orbit control

--- src/components/macbook/index.tsx
+++ src/components/macbook/index.tsx
@@ -15,27 +15,33 @@ const MacbookView = () => {
   return (
     <div className={style.macbookView}>
       <Canvas>
-        {/* <OrbitControls /> */}
-        <PresentationControls global polar={[0, 0.9]} azimuth={[-0.5, 0.5]}>
-          <group scale={0.7}>
-            <Html
-              wrapperClass={style.macbookIframe}
-              position={[0, 0.3, -2.3]}
-              transform
-              distanceFactor={1.2}
-              rotation={[-0.34, 0, 0]}
-              scale={1.085}
-            >
-              <iframe src="https://trungkhai.vercel.app/"></iframe>
-            </Html>
-            <MacbookModel
-              position={[0, -1.2, 0]}
-              // rotation={[0, 0.7, 0]}
-              scale={14}
-            />
-          </group>
-          <Environment preset="warehouse" />
-        </PresentationControls>
+        <OrbitControls
+          minPolarAngle={Math.PI / 100} // Giới hạn góc xoay tối thiểu theo trục y
+          maxPolarAngle={Math.PI / 2} // Giới hạn góc xoay tối đa theo trục y
+          minAzimuthAngle={-Math.PI / 2} // Giới hạn góc xoay tối thiểu theo trục x
+          maxAzimuthAngle={Math.PI / 2} // Giới hạn góc xoay tối đa theo trục x
+          rotateSpeed={0.3}
+        />
+        {/* <PresentationControls global polar={[0, 0.9]} azimuth={[-0.5, 0.5]}> */}
+        <group scale={0.7}>
+          <Html
+            wrapperClass={style.macbookIframe}
+            position={[0, 0.3, -2.25]}
+            transform
+            distanceFactor={1.2}
+            rotation={[-0.34, 0, 0]}
+            scale={1.087}
+          >
+            <iframe src="https://trungkhai.vercel.app/"></iframe>
+          </Html>
+          <MacbookModel
+            position={[0, -1.2, 0]}
+            // rotation={[0, 0.7, 0]}
+            scale={14}
+          />
+        </group>
+        <Environment preset="warehouse" />
+        {/* </PresentationControls> */}
         <ambientLight intensity={0} />
       </Canvas>
     </div>
